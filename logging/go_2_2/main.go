98e5e64f4af901c814899e53be7a472d0b6f69c4
Mehrdad <m.mirsamie@gmail.com>
2021-06-28 22:32:56 +0430

    removed unused items

--- logging/go_2_2/main.go
+++ logging/go_2_2/main.go
@@ -11,24 +11,10 @@ type Logger struct {
 	Output     io.Writer
 }
 
-func (receiver Logger) Log(a ...interface{}) {
-	if receiver.OutputText {
-		fmt.Fprintln(receiver.Output, a...)
-	}
-}
-
 func (receiver Logger) Logf(format string, a ...interface{}) {
 	if receiver.OutputText {
 		fmt.Fprintf(receiver.Output, format, a...)
 	}
-}
-
-func (receiver Logger) Begin(a ...interface{}) {
-	receiver.Log("BEGIN")
-}
-
-func (receiver Logger) End(a ...interface{}) {
-	receiver.Log("END")
 }
 
 func main() {
