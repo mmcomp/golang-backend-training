052b2a1d582f80b2914979c689255a2887c58c0f
Mehrdad <m.mirsamie@gmail.com>
2021-06-29 17:13:38 +0430

    finish 3.1

---
+++ webserver/go_3_1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/mmcomp/go_log"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello world")
+	go_log.Log("New Req")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	go_log.Log(http.ListenAndServe(":8080", nil))
+}
