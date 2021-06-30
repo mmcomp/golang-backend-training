a416a3d8e12ec4f1e8bc5abb1da6e8fa38bedbbf
Mehrdad <m.mirsamie@gmail.com>
2021-06-30 12:45:05 +0430

    finished logging 3.6

---
+++ logging/go_3_6/go.mod
@@ -0,0 +1,5 @@
+module go_3_6
+
+go 1.12
+
+require github.com/mmcomp/go-log v0.0.0-20210630064542-e1fcf68cc72e
---
+++ logging/go_3_6/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	l "github.com/mmcomp/go-log"
+)
+
+func main() {
+	l.Default.Begin()
+
+	l.Default.End()
+}
