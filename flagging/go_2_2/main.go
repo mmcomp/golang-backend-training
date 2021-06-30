24550762535734f5e0d487c6a278c975303680a2
Mehrdad <m.mirsamie@gmail.com>
2021-06-30 09:52:16 +0430

    finished flagging 2.3

---
+++ flagging/go_2_2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var name string
+	var shhh bool
+	flag.StringVar(&name, "name", "NoName", "It is your name!")
+	flag.BoolVar(&shhh, "shhh", false, "It is used to SHHH the output!")
+	flag.Parse()
+	if !shhh {
+		fmt.Printf("Hello %s\n", name)
+	}
+}
