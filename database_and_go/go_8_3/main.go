f03faed71ac849ef2889522209cecbf98d20089a
mmcomp <m.mirsamie@gmail.com>
2021-07-04 09:24:01 +0430

    finished 8.3

---
+++ database_and_go/go_8_3/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/lib/pq"
+	go_log "github.com/mmcomp/go-log"
+)
+
+func main() {
+	connector, err := pq.NewConnector("user=mehrdad password=123456 dbname=mehrdad sslmode=disable")
+	if err != nil {
+		go_log.Logf("Error Connector : %s", err.Error())
+		return
+	}
+	db := sql.OpenDB(connector)
+	var id int64
+	var name string = "AmirArsalan"
+	var email string = "a.mirsamie@gmail.com"
+	db.QueryRow("insert into accounts (name, email) values ($1, $2) returning id", name, email).Scan(&id)
+	rows, err := db.Query("select * from accounts where id = $1", id)
+	if err != nil {
+		go_log.Logf("Error Query : %s", err.Error())
+		return
+	}
+	var selectedId int64
+	var selectedName string
+	var selectedEmail string
+	var when_created string
+	for rows.Next() {
+		rows.Scan(&selectedId, &selectedName, &selectedEmail, &when_created)
+		go_log.Logf("id = %d, name = %q, email = %q, when_created = %q", selectedId, selectedName, selectedEmail, when_created)
+	}
+	defer db.Close()
+}
