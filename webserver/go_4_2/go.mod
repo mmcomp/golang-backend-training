7703e5b95f6e2dde48bc7809f068301acf2e7e4c
Mehrdad <m.mirsamie@gmail.com>
2021-07-06 06:10:04 +0430

    ADDED Log

--- webserver/go_4_2/go.mod
+++ webserver/go_4_2/go.mod
@@ -2,6 +2,4 @@ module github.com/mmcomp/golang-backend-training
 
 go 1.12
 
-require (
-	github.com/mmcomp/go-log v0.0.0-20210703093350-0ceee54a309c 
-)
+require github.com/mmcomp/go-log v0.0.0-20210706013848-a6874dd640b9
--- webserver/go_4_2/main.go
+++ webserver/go_4_2/main.go
@@ -8,19 +8,33 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	go_log.OutputFn("BEGIN")
+	defer go_log.OutputFn("END")
 	fmt.Fprint(w, "Hello world")
 	go_log.Log("New Req")
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query();
+	go_log.OutputFn("BEGIN")
+	defer go_log.OutputFn("END")
+	query := r.URL.Query()
 	name := query.Get("name")
+	if name == "" {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		go_log.Error("No Name Error Happend")
+		return
+	}
 	fmt.Fprintf(w, "Hello %s", name)
 	go_log.Logf("New Req to hello name = %q", name)
 }
 
 func main() {
+	go_log.OutputFn("BEGIN")
+	defer go_log.OutputFn("END")
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/hello", helloHandler)
-	go_log.Log(http.ListenAndServe(":8080", nil))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		go_log.Error("Error from Web Server", err)
+	}
 }
