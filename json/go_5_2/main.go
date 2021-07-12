cdf8a596c6cea4296f1f3e44ba454811da680f84
mmcomp <m.mirsamie@gmail.com>
2021-07-12 13:47:53 +0430

    fixed accourding to comments

--- json/go_5_2/main.go
+++ json/go_5_2/main.go
@@ -1,35 +1,68 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"encoding/json"
+	"net/http"
 
 	go_log "github.com/mmcomp/go-log"
 )
 
+func jsonError(w http.ResponseWriter, error []byte, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(code)
+	_, err := w.Write(error)
+	if err != nil {
+		go_log.Error("Write Json Error to output Error", err)
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	go_log.Begin()
+	defer go_log.End()
 	response := make(map[string]string)
 	response["msg"] = "Hello world"
-	jsonResponse, _ := json.MarshalIndent(response, "", "")
+	jsonResponse, err := json.MarshalIndent(response, "", "")
+	if err != nil {
+		go_log.Error("Error Marshaling", err)
+		response["msg"] = "Error in Marshaling output!"
+		errorJsonResponse, _ := json.MarshalIndent(response, "", "")
+		jsonError(w, errorJsonResponse, http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, string(jsonResponse))
+	w.Write(jsonResponse)
 	go_log.Log("New Req")
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query();
+	go_log.Begin()
+	defer go_log.End()
+	query := r.URL.Query()
 	name := query.Get("name")
 	response := make(map[string]string)
 	response["msg"] = "Hello " + name
-	jsonResponse, _ := json.MarshalIndent(response, "", "")
+	jsonResponse, err := json.MarshalIndent(response, "", "")
+	if err != nil {
+		go_log.Error("Error Marshaling", err)
+		response["msg"] = "Error in Marshaling output!"
+		errorJsonResponse, _ := json.MarshalIndent(response, "", "")
+		jsonError(w, errorJsonResponse, http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, string(jsonResponse))
+	w.Write(jsonResponse)
 	go_log.Logf("New Req to hello name = %q", name)
 }
 
 func main() {
+	go_log.Begin()
+	defer go_log.End()
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/hello", helloHandler)
-	go_log.Log(http.ListenAndServe(":8080", nil))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		go_log.Error("Http Error", err)
+	}
+	go_log.Log("Http server stopped!")
 }
