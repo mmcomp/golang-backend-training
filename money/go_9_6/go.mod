daee02c73d14b8701276b0b85e0feca411ec8baf
mirsamie <m.mirsamie@gmail.com>
2021-07-14 20:01:20 +0430

    used money package

--- money/go_9_6/go.mod
+++ money/go_9_6/go.mod
@@ -1,3 +1,5 @@
 module money_9_1
 
 go 1.16
+
+require github.com/mmcomp/go-money v0.0.0-20210714141512-23bcaa3c8bf4
--- money/go_9_6/main.go
+++ money/go_9_6/main.go
@@ -2,102 +2,12 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
+
+	"github.com/mmcomp/go-money"
 )
 
-type CAD struct {
-	cents int64
-}
-
-func Cents(n int64) CAD {
-	cad := CAD{
-		cents: n,
-	}
-	return cad
-}
-
-func ParseCAD(s string) (CAD, error) {
-	s = strings.Replace(s, "$", "", 1)
-	s = strings.Replace(s, "CAD", "", 1)
-	s = strings.Replace(s, "¢", "", 1)
-	s = strings.Replace(s, ",", "", -1)
-	s = strings.Replace(s, ".", "", -1)
-	s = strings.Replace(s, " ", "", -1)
-	cad := CAD{
-		cents: 0,
-	}
-	intValue, err := strconv.ParseInt(s, 10, 64)
-	if err != nil {
-		return cad, err
-	}
-	cad.cents = intValue
-	return cad, nil
-}
-
-func (receiver CAD) Abs() CAD {
-	if receiver.cents < 0 {
-		receiver.cents *= -1
-	}
-	return receiver
-}
-
-func (receiver CAD) Add(other CAD) CAD {
-	receiver.cents += other.cents
-	return receiver
-}
-
-func (receiver CAD) AsCents() int64 {
-	return receiver.cents
-}
-
-func (receiver CAD) CanonicalForm() (int64, int64) {
-	dollars := receiver.cents / 100
-	cents := receiver.cents % 100
-
-	return dollars, cents
-}
-
-func (receiver CAD) Mul(scalar int64) CAD {
-	receiver.cents *= scalar
-	return receiver
-}
-
-func (receiver CAD) Sub(other CAD) CAD {
-	receiver.cents -= other.cents
-	return receiver
-}
-
-func (receiver CAD) GoString() string {
-	result := fmt.Sprintf("main.cents(%d)", receiver.cents)
-	return result
-}
-
-func (receiver CAD) String() string {
-	var sign int8 = 1
-	if receiver.cents < 0 {
-		sign = -1
-	}
-	receiver = receiver.Abs()
-	dollars, cents := receiver.CanonicalForm()
-	var centsStr string = fmt.Sprintf("%d", cents)
-	if cents < 10 {
-		centsStr = "0" + centsStr
-	}
-	result := fmt.Sprintf("$%d.%s", dollars, centsStr)
-	if sign < 0 {
-		result = "-" + result
-	}
-	return result
-}
-
-func (receiver CAD) MarshalJSON() ([]byte, error) {
-	result := receiver.String()
-	return []byte(result), nil
-}
-
 func main() {
-	money, err := ParseCAD("$1.23")
+	money, err := money.ParseCAD("$1.23")
 	if err != nil {
 		fmt.Println(err.Error())
 		return
